15df6775c4fe475187fd3c39926d3254ea36a0a0
techay-mihir-simform <70096980+techay-mihir-simform@users.noreply.github.com>
2022-11-25 17:18:02 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,2 +1,24 @@
-#!/bin/bash
-aws ecs register-task-definition  --region "us-east-1" --family "file360" --container-definitions "[{\"name\":\"sleep\",\"image\":\"busybox\",\"cpu\":10,\"command\":[\"sleep\",\"360\"],\"memory\":10,\"essential\":true}]" --network-mode "awsvpc"   --requires-compatibilities "FARGATE" --cpu "512" --memory "1024" 
+#! /bin/bash
+file_name=`git whatchanged --diff-filter=A  --pretty=""  --name-only`
+echo $file_name
+string_array=($file_name)
+cd_name=""
+# echo ${string_array[1]}
+for i in ${string_array[@]}
+do
+    if [[ $i == *.py ]]
+    then
+        echo "hello"+$i
+        # mkdir $i+"tets"
+        file_name=$i
+        familly_name=`basename $file_name .py`
+        echo $familly_name
+        cd=`cat container_defination.json`
+        echo $cd
+        new_cd=$(echo $cd | jq --arg cm "${file_name}" '.containerDefinitions[0].command[1]= $cm')
+        echo $new_cd
+        only_container=$(echo $new_cd | jq -r '.containerDefinitions')
+        aws ecs register-task-definition  --region "us-east-1" --family "${familly_name}" --container-definitions "${only_container}" --network-mode "awsvpc"   --requires-compatibilities "FARGATE" --cpu "512" --memory "1024"  
+    fi
+done
+
